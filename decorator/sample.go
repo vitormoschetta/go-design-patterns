2386b3ebdba7746f10e523fa33b1b30a4ff3b7b4
Vitor Moschetta <vitor.moschetta@mercadolivre.com>
2024-01-06 17:34:03 -0400

    add decorator

---
+++ decorator/sample.go
@@ -0,0 +1,64 @@
+package decorator
+
+import "fmt"
+
+// INTERFACE DE ENVIO DE E-MAIL
+type IEmailSender interface {
+	SendEmail(to, subject, body string) error
+}
+
+// IMPLEMENTAÇÃO DE ENVIO DE E-MAIL PARA UM SERVIDOR SMTP
+type SmtpEmailSender struct{}
+
+func (s *SmtpEmailSender) SendEmail(to, subject, body string) error {
+	// Implementação de envio de e-mail para um servidor SMTP
+	fmt.Printf("Enviando e-mail para %s com assunto '%s' e corpo '%s'\n", to, subject, body)
+	return nil
+}
+
+// SERVIÇO DE USUÁRIO QUE UTILIZA O ENVIO DE E-MAIL
+type UserService struct {
+	EmailSender IEmailSender
+}
+
+func NewUserService(emailSender IEmailSender) *UserService {
+	return &UserService{EmailSender: emailSender}
+}
+
+func (u *UserService) RegisterUser(name, email string) error {
+	return u.EmailSender.SendEmail(email, "Bem-vindo!", "Obrigado por se registrar!")
+}
+
+// DECORATOR QUE ADICIONA UM COMPORTAMENTO AO ENVIO DE E-MAIL
+type IDecorator interface {
+	IEmailSender // Herda os contratos da interface IEmailSender
+	AddedBehavior() string
+}
+
+// DECORATOR QUE ADICIONA UM COMPORTAMENTO AO ENVIO DE E-MAIL
+type DecoratorEmailSender struct {
+	IEmailSender
+}
+
+func (d *DecoratorEmailSender) AddedBehavior() string {
+	return "Comportamento adicionado"
+}
+
+func (d *DecoratorEmailSender) SendEmail(to, subject, body string) error {
+	fmt.Println("Adicionando comportamento")
+	return d.IEmailSender.SendEmail(to, subject, body)
+}
+
+// SERVIÇO DE USUÁRIO QUE UTILIZA O DECORATOR
+type UserService2 struct {
+	Decorator IDecorator
+}
+
+func NewUserService2(decorator IDecorator) *UserService2 {
+	return &UserService2{Decorator: decorator}
+}
+
+func (u *UserService2) RegisterUser(name, email string) error {
+	u.Decorator.AddedBehavior() // Comportamento adicionado
+	return u.Decorator.SendEmail(email, "Bem-vindo!", "Obrigado por se registrar!")
+}
---
+++ decorator/sample_test.go
@@ -0,0 +1,48 @@
+package decorator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MockEmailSender struct {
+	SendEmailFunc func(to, subject, body string) error
+}
+
+func (m *MockEmailSender) SendEmail(to, subject, body string) error {
+	return m.SendEmailFunc(to, subject, body)
+}
+
+func TestSendEmail_WithoutDecorator(t *testing.T) {
+	// arrange
+	mockEmailSender := &MockEmailSender{
+		SendEmailFunc: func(to, subject, body string) error {
+			return nil
+		},
+	}
+	userService := NewUserService(mockEmailSender)
+
+	// act
+	err := userService.RegisterUser("John Doe", "user@mail.com")
+
+	// assert
+	assert.Nil(t, err)
+}
+
+func TestSendEmail_WithDecorator(t *testing.T) {
+	// arrange
+	mockEmailSender := &MockEmailSender{
+		SendEmailFunc: func(to, subject, body string) error {
+			return nil
+		},
+	}
+	decorator := &DecoratorEmailSender{IEmailSender: mockEmailSender}
+	userService := NewUserService2(decorator)
+
+	// act
+	err := userService.RegisterUser("John Doe", "user@mail.com")
+
+	// assert
+	assert.Nil(t, err)
+}
